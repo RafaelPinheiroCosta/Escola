09e69db5c9b9e667a8e8112da900581daa8d547d
RafaelPinheiroCosta <rafaelcost@gmail.com>
2023-07-22 12:49:30 -0300

    Metodos professor

--- src/Professor.java
+++ src/Professor.java
@@ -1,19 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Professor extends Usuario{
+    List<Aluno> listaAlunos = new ArrayList<>();
 
     Professor(String nome, String curso) {
         super(nome, curso, "Professor");
     }
+    public void criarTurma(String turma){
+        super.turma = turma;
 
-    public void criarTurma(){
-
+        System.out.println("Turma criada com sucesso!!");
     }
-    public void matricularAlunos(){
+    public void matricularAluno(Aluno aluno){
+        aluno.turma = super.turma;
+        listaAlunos.add(aluno);
 
+        System.out.println("Aluno matriculado com sucesso!!");
     }
-    public void darNotas(){
+    public void darNotas(Aluno aluno, int nota){
+        aluno.receberNota(nota);
 
+        System.out.println("Nota Atribuida com sucesso!!");
     }
-    public void darFaltas(){
+    public void darFaltas(Aluno aluno, int faltas){
+        aluno.receberFaltas(faltas);
 
+        System.out.println("Falta Atribuida com sucesso!!");
+    }
+    public void reservarSala(String sala){
+        super.sala = sala;
+        for(Aluno aluno: listaAlunos){
+            aluno.sala = sala;
+        }
+        System.out.println("Sala Reservada com sucesso!!");
     }
 }
