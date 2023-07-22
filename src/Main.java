015ed60599b88b3fbb1630a1207367bc2f974b16
RafaelPinheiroCosta <rafaelcost@gmail.com>
2023-07-22 15:20:23 -0300

    Metodos professor 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Professor professor = new Professor("Rafael", "Fundamentos de Java");
+
+        professor.criarTurma("FJ2023");
+
+        professor.matricularAluno(new Aluno("João", professor.curso));
+        professor.matricularAluno(new Aluno("Maria", professor.curso));
+        professor.matricularAluno(new Aluno("José", professor.curso));
+        professor.matricularAluno(new Aluno("XXXX", professor.curso));
+        professor.matricularAluno(new Aluno("YYYY", professor.curso));
+
+        professor.reservarSala("Sala 1");
+
+       for(Aluno aluno: professor.listaAlunos){
+           professor.darNotas(aluno,50);
+           professor.darFaltas(aluno,2);
+       }
+
+       professor.apresentarInformacoes();
+
+        for(Aluno aluno: professor.listaAlunos){
+            aluno.apresentarInformacoes();
+        }
+
 
     }
 }
